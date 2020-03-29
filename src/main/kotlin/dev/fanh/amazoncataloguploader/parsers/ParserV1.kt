b566e57deb96dcc0b1b0e2dd80adce2de95d246d
Fabio Navarrete <fabioanh@gmail.com>
2020-03-29 18:16:35 +0400

    Parser for species 80% finished. Missing images and Endangered status

--- src/main/kotlin/dev/fanh/amazoncataloguploader/parsers/ParserV1.kt
+++ src/main/kotlin/dev/fanh/amazoncataloguploader/parsers/ParserV1.kt
@@ -1,9 +1,6 @@
 package dev.fanh.amazoncataloguploader.parsers
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonElement
-import com.google.gson.JsonObject
+import com.google.gson.*
 import dev.fanh.amazoncataloguploader.data.*
 import dev.fanh.amazoncataloguploader.utils.languageFromJsonV1
 
@@ -12,7 +9,30 @@ public class ParserV1 : Parser {
     private val gson: Gson = GsonBuilder().serializeNulls().create()
 
     override fun parseSpecies(filename: String): Species {
-        TODO("Not yet implemented")
+        val speciesDataObject = gson.fromJson(jsonFileReader(filename), JsonObject::class.java)
+        return Species(version = this.version.name,
+                id = speciesDataObject.get("_id").asString,
+                behaviour = speciesDataObject.get("behaviorApprovedInUse")?.asJsonObject?.get("behavior")?.asJsonObject?.get("behaviorUnstructured")?.asString,
+                commonNames = extractCommonNames(speciesDataObject.get("commonNames")),
+                description = speciesDataObject.get("abstractApprovedInUse")?.asJsonObject?.get("abstract")?.asString ?: throw Exception("Description should not be null"),
+                endangeredStatus = extractEndangeredStatus(speciesDataObject.get("threatStatusApprovedInUse")), //speciesDataObject.get("threatStatusValue")
+                feeding = speciesDataObject.get("feedingApprovedInUse")?.asJsonObject?.get("feeding")?.asJsonObject?.get("feedingUnstructured")?.asString,
+                fullDescription = speciesDataObject.get("fullDescriptionApprovedInUse")?.asJsonObject?.get("fullDescription")?.asJsonObject?.get("fullDescriptionUnstructured")?.asString ?: throw Exception("Full Description should not be null"),
+                habitat = speciesDataObject.get("habitatsApprovedInUse")?.asJsonObject?.get("habitats")?.asJsonObject?.get("habitatsUnstructured")?.asString,
+                imageURLs = extractImages(speciesDataObject.get("ancillaryDataApprovedInUse")?.asJsonObject?.get("ancillaryData")?.asJsonArray),
+                lifecycle = speciesDataObject.get("lifeCycleApprovedInUse")?.asJsonObject?.get("lifeCycle")?.asJsonObject?.get("lifeCycleUnstructured")?.asString,
+                lifeForm = speciesDataObject.get("lifeFormApprovedInUse")?.asJsonObject?.get("lifeForm")?.asJsonObject?.get("lifeFormUnstructured")?.asString,
+                migration = speciesDataObject.get("migratoryApprovedInUse")?.asJsonObject?.get("migratory")?.asJsonObject?.get("migratoryUnstructured")?.asString,
+                reproduction = speciesDataObject.get("reproductionApprovedInUse")?.asJsonObject?.get("reproduction")?.asJsonObject?.get("reproductionUnstructured")?.asString,
+                scientificName = speciesDataObject.get("scientificNameSimple")?.asString ?: throw Exception("Scientific Name should not be null"))
+    }
+
+    private fun extractImages(imageElements: JsonArray?): List<String>? {
+        TODO()
+    }
+
+    private fun extractEndangeredStatus(endangeredStatusElements: JsonElement): List<LocalisedValue>? {
+        TODO()
     }
 
     override fun parseSpeciesList(filename: String): SpeciesList {
