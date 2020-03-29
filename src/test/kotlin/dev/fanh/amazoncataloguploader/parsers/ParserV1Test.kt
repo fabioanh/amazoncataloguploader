0393654a4758be45edb8763ef01cc996c8df5e20
Fabio Navarrete <fabioanh@gmail.com>
2020-03-29 17:43:02 +0400

    Tests for species parsing addapted accordingly with accurate test data

--- src/test/kotlin/dev/fanh/amazoncataloguploader/parsers/ParserV1Test.kt
+++ src/test/kotlin/dev/fanh/amazoncataloguploader/parsers/ParserV1Test.kt
@@ -10,9 +10,9 @@ class ParserV1Test {
 
     @Test
     fun parseMinimumSpecies() {
-        val result = parser.parseSpecies("testSpecies.json")
+        val result = parser.parseSpecies("testMinimumSpecies.json")
         val expectedSpecies = getExpectedSpecies()
-        assertEquals(expectedSpecies, result, "Species object was not parsed properly")
+        assertEquals(expectedSpecies, result, "Minimum Species object was not parsed properly")
     }
 
     @Test
@@ -44,7 +44,9 @@ class ParserV1Test {
     private fun getExpectedSpecies(): Species {
         return Species(version = "V1",
                 id = "idAnimal1",
-                commonNames = listOf(LanguagedValue("stripped jaguar", "en")),
+                commonNames = listOf(LanguagedValue("stripped jaguar", "en"),
+                        LanguagedValue("jaguar rayado", "es"),
+                        LanguagedValue("Jaguarayado", "unknown")),
                 description = "The stripped jaguar is a cat that resembles a Zebra, beware!",
                 fullDescription = "The stripped jaguar is very difficult to spot as it hides in the middle of Zebra groups. Some tribes refer to it as a wolf in sheep's clothing",
                 scientificName = "Jaguarus Felinus Rayadus")
@@ -57,13 +59,18 @@ class ParserV1Test {
                 description = "The stripped jaguar is a cat that resembles a Zebra, beware!",
                 fullDescription = "The stripped jaguar is very difficult to spot as it hides in the middle of Zebra groups. Some tribes refer to it as a wolf in sheep's clothing",
                 scientificName = "Jaguarus Felinus Rayadus",
-                behaviour = "This animal behaves wildly",
-                endangeredStatus = listOf(LocalisedValue("En Peligro", "co")),
+                behaviour = "This animal behaves wildly. Simultánea",
+                endangeredStatus = listOf(LocalisedValue("En Peligro", "co"), LocalisedValue("En Peligro Crítico (CR)", "global")),
                 feeding = "Eats only aquatic zebras from the Peruvian Amazon",
                 habitat = "Amazon river bank",
-                imageURLs = listOf("https://d2ouvy59p0dg6k.cloudfront.net/img/original/original_ww2137556.jpg"),
+                imageURLs = listOf("https://d2ouvy59p0dg6k.cloudfront.net/img/original/original_ww2137556.jpg",
+                        "https://url1.com/img1.jpg",
+                        "https://url5.com/img5.jpg",
+                        "https://url2.com/img2.jpg",
+                        "https://url3.com/img3.jpg",
+                        "https://url4.com/img4.jpg"),
                 lifecycle = "Its reproduction cycle is tied to the pink dolphins dancing habits",
-                lifeForm = "It's mostly active during the evenings",
+                lifeForm = "Hábito: It's mostly active during the evenings",
                 migration = "Doesn't present migratory habits as it can eat aquatic zebras all year long",
                 reproduction = "Creates nests on top of the tallest Ceibas")
     }
